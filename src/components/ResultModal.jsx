c613a4cbc7d998a3203933ed0d0472a0f32631f9
Eslam Saeed <eslamsaeed298@gmail.com>
2025-08-06 04:38:06 +0300

    calculate time remaining

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,10 @@
 import { useImperativeHandle, useRef } from "react"
 
-export default function ResultModal({ref, result, targetTime }) {
+export default function ResultModal({ref, targetTime, remainingTime, onReset }) {
     const dialogRef = useRef();
+
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
     useImperativeHandle(ref, () => {
         return {
@@ -12,10 +15,10 @@ export default function ResultModal({ref, result, targetTime }) {
     });
 
     return <dialog className="result-modal" ref={dialogRef}>
-        <h2>You { result }</h2>
+        {userLost && <h2>You Lost</h2>}
         <p>The Target Time was <strong>{ targetTime } seconds.</strong></p>
-        <p>You Stopped the Timer with <strong>X seconds left</strong></p>
-        <form action="dialog">
+        <p>You Stopped the Timer with <strong>{formattedRemainingTime} seconds left</strong></p>
+        <form action="dialog" onSubmit={onReset}>
             <button>Close</button>
         </form>
     </dialog>
